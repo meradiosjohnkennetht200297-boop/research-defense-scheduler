9a7be76bd48df1959ce6dd6966502238b70dccdc
John Kenneth Meradios <meradiosjohnkennetht200297@gmail.com>
2026-08-19 12:43:21 +0800

    Add month-aware public home calendar

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,5 @@
 import MinimalHomeV2 from '../home-minimal-v2'
 
-export default MinimalHomeV2
+export default function HomePage({ searchParams }: { searchParams: Promise<{ month?: string }> }) {
+  return <MinimalHomeV2 searchParams={searchParams} />
+}
